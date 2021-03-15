c016271f4d808a47c51538ecec0fe63532ec9cf7
jankludkiewicz <jankludkiewicz@gmail.com>
2021-03-15 23:07:40 +0100

    Update startattempt_form_script.js

--- startattempt_form_script.js
+++ startattempt_form_script.js
@@ -2,4 +2,5 @@ function onRangeMouseUp() {
 	var rangeElement = document.getElementById('questionsno');
 	document.getElementById('questionsnodisplay').innerHTML = rangeElement.value;
 	console.log(document.getElementById('mform1_sQ136YZdB9gpVBK'));
+	console.log(rangeElement.value);
 }
