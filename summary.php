2ecead055050c8430ef72f4b8c4656c80aefaba8
jankludkiewicz <jankludkiewicz@gmail.com>
2021-03-21 10:10:30 +0000

    Update summary.php

--- summary.php
+++ summary.php
@@ -22,6 +22,7 @@
  */
  
 require_once(dirname(dirname(dirname(__FILE__))) . '/config.php');
+require_once($CFG->libdir . '/questionlib.php');
 require_once(dirname(__FILE__) . '/locallib.php');
 
 $id = required_param('id', PARAM_INT); // Instance id.
