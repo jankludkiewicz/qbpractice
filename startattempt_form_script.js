11a3b0264144d6d46d3e2c6cfccbeceec7248d17
jankludkiewicz <jankludkiewicz@gmail.com>
2021-03-30 19:22:43 +0200

    Update startattempt_form_script.js

--- startattempt_form_script.js
+++ startattempt_form_script.js
@@ -1,6 +1,6 @@
 var noofquestions = 0;
 
-var formElements = document.querySelectorAll("input[type='checkbox' name^='subcategories']");
+var formElements = document.querySelectorAll("input[type='checkbox'][name^='subcategories']");
 
 console.log(formElements);
 for (var i=0; i<formElements.length; i++) {
