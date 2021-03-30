af5903a7915a98bec7545e945874191ed12199bd
jankludkiewicz <jankludkiewicz@gmail.com>
2021-03-30 19:30:55 +0200

    Update startattempt_form_script.js

--- startattempt_form_script.js
+++ startattempt_form_script.js
@@ -14,12 +14,10 @@ function eventListener(a) {
 	questionElement = document.querySelector("input[name='"+a.name+"_noofquestions']");
 	if (a.checked) noofquestions += questionElement.value;
 	else noofquestions -= questionElement.value;
-	onRangeMouseUp();
+	updateRangeInfo();
 }
 
-function onRangeMouseUp() {
+function updateRangeInfo() {
 	var rangeElement = document.getElementById('questionsno');
-	document.getElementById('questionsnodisplay').innerHTML = rangeElement.value;
-	var formelements = document.querySelectorAll("input[name^='subcategories']")
-	console.log(formelements);
+	document.getElementById('questionsnodisplay').innerHTML = rangeElement.value+" / "+noofquestions;
 }
