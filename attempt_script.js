dd22321f63bb22206d769d7814b9277952a23650
jankludkiewicz <jankludkiewicz@gmail.com>
2021-04-02 22:39:52 +0200

    Update attempt_script.js

--- attempt_script.js
+++ attempt_script.js
@@ -2,7 +2,7 @@ M.util.js_pending('random606772a4ef9cb1');
 Y.use('core_question_flags', function(Y) {
 	M.core_question_flags.add_listener(flipFlag);
 	M.util.js_complete('random606772a4ef9cb1'); 
-}
+});
 
 M.core_question_flags.add_listener(flipFlag);
 
