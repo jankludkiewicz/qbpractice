352841b452931236adf87b1ed77ee9766dfab46e
jankludkiewicz <jankludkiewicz@gmail.com>
2021-04-02 22:31:53 +0200

    Update attempt_script.js

--- attempt_script.js
+++ attempt_script.js
@@ -1,18 +1,8 @@
-/*var flag = document.querySelector('div.questionflag').childNodes[1];
-console.log(flag);
-flag.addEventListener("change", flipFlag);*/
-
-document.querySelector('input[id="changer"]').addEventListener("click", consoleShow);
+M.core_question_flags.add_listener(flipFlag);
 
 function flipFlag() {
 	var flag = document.querySelector('input.questionflagvalue');
 	var questionflagpostdata = document.querySelector('input.questionflagpostdata').value;
 	var toggleurl = 'toggleflag.php?'+questionflagpostdata;
 	$.ajax({url: toggleurl, success: function(result){alert(result);}});
-}
-
-function consoleShow() {
-	var flag = document.querySelector('input.questionflagvalue');
-	flipFlag();
-	console.log(flag);
 }
