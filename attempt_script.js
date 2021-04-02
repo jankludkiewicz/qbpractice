699edd3d65450b1207615891c5e7272b6c40ddd1
jankludkiewicz <jankludkiewicz@gmail.com>
2021-04-02 21:07:55 +0200

    Update attempt_script.js

--- attempt_script.js
+++ attempt_script.js
@@ -3,6 +3,7 @@ flag.addEventListener("change", flipFlag);
 
 function flipFlag() {
 	var flag = document.querySelector('input.questionflagvalue');
+	console.log(flag);
 	var newstate = flag.value=='1'?true:false;
 	var qid = document.getElementById('questionid').value;
 	var toggleurl = 'toggleflag.php?qid='+qid+'&newstate='+newstate
