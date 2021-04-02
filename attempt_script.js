c3edaaca41e59de61be1c6c551f1dfbc0e09fa45
jankludkiewicz <jankludkiewicz@gmail.com>
2021-04-02 21:34:51 +0200

    Update attempt_script.js

--- attempt_script.js
+++ attempt_script.js
@@ -8,7 +8,7 @@ function flipFlag() {
 	var flag = document.querySelector('input.questionflagvalue');
 	var questionflagpostdata = document.querySelector('input.questionflagpostdata').value;
 	var toggleurl = 'toggleflag.php?'+questionflagpostdata;
-	$.ajax({url: toggleurl, success: alert(result)});
+	$.ajax({url: toggleurl, success: function(result){alert(result);}});
 }
 
 function consoleShow() {
