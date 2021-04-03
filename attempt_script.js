64e5fdb528f4c51e609998bbd1df1a0f3408af1a
jankludkiewicz <jankludkiewicz@gmail.com>
2021-04-03 17:56:09 +0200

    Update attempt_script.js

--- attempt_script.js
+++ attempt_script.js
@@ -7,7 +7,7 @@ Y.use('core_question_flags', function(Y) {
 YUI().use('node-base', function(Y) {
     function init() {
         Y.all("input[name='finish']").on('click', function(e) {
-            var args = {'message':'Are you sure you want to finish current session? You will not be able to attempt this session any more. This session may be accessed through summary table for future reference.'};
+            var args = {'message':'Are you sure you want to finish current session? All the finished and unfinished progress is going to be saved and you will not be able to attempt this session any more. Results can be accessed through summary table for future reference.'};
             M.util.show_confirm_dialog(e, args);
             return false;
         });
