7a74abc7c17e6a6da0cff82526f54fc18aa51f51
jankludkiewicz <jankludkiewicz@gmail.com>
2021-04-03 13:01:41 +0200

    Update attempt_script.js

--- attempt_script.js
+++ attempt_script.js
@@ -1,13 +1,17 @@
 M.util.js_pending('qbpractice_flags');
 Y.use('core_question_flags', function(Y) {
-	M.core_question_flags.add_listener(ajaxDBFlagQuery);
+	M.core_question_flags.add_listener(manageFlags);
 	M.util.js_complete('qbpractice_flags'); 
 });
 
-function ajaxDBFlagQuery() {
-	var flag = document.querySelector('input.questionflagvalue');
+function manageFlags() {
+	// Send AJAX query
 	var questionid = document.querySelector('input[id="questionid"]').value;
-	var newstate = flag.value;
-	var toggleurl = 'toggleflag.php?qid='+questionid+'&newstate='+newstate;
-	$.ajax({url: toggleurl});
+	var newstate = document.querySelector('input.questionflagvalue').value;
+	$.ajax({url: 'toggleflag.php?qid='+questionid+'&newstate='+newstate});
+	
+	// Change flag in navigation menu
+	qnbutton.classList.remove('flagged');
+	var qnbutton = document.querySelector('a.qnbutton.thispage');
+	if (newstate == 1) qnbutton.classList.add('flagged');
 }
