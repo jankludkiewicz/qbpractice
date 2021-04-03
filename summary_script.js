4360c0eb89d75df6c3a5bb42bf5496fd1669e69d
jankludkiewicz <jankludkiewicz@gmail.com>
2021-04-03 17:48:02 +0200

    Update summary_script.js

--- summary_script.js
+++ summary_script.js
@@ -1,6 +1,6 @@
 YUI().use('node-base', function(Y) {
     function init() {
-        Y.all(".clear_practice_history").on('click', function(e) {
+        Y.all("#clear_practice_history").on('click', function(e) {
             var args = {'url':e.currentTarget.get('href'), 'message':'Are you sure you want to clear all <b>practice history</b>? All question attempts including flags, sessions and its results will be lost.'};
             M.util.show_confirm_dialog(e, args);
             return false;
