b09d6d820b72ca8c1839e143ce9c289d47a06248
jankludkiewicz <jankludkiewicz@gmail.com>
2021-04-03 16:05:23 +0200

    cv

--- summary.php
+++ summary.php
@@ -94,7 +94,7 @@ if (!empty($sessions)) {
 	$html .= html_writer::link($actionurl, $label, array(null));
 }
 
-html .= html_writer::empty_tag('br');
+$html .= html_writer::empty_tag('br');
 
 $actionurl = new moodle_url($context->get_url());
 $label = html_writer::tag('span', get_string('backtocourse', 'block_qbpractice'));
