62ca918f849d2f4f53c39500ada62d0b07af18ad
jankludkiewicz <jankludkiewicz@gmail.com>
2021-04-03 13:42:12 +0200

    Update summary.php

--- summary.php
+++ summary.php
@@ -47,8 +47,6 @@ $table->align = array('left', 'left');
 $table->size = array('', '');
 $table->data = array();
 
-qbpractice_session_finish();
-
 $sessions = $DB->get_records('qbpractice_session', array('userid' => $USER->id));
 
 $i = 1;
