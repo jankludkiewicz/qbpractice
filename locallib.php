0b4e7033ba01c26eb07d107535d4285b9ba39744
jankludkiewicz <jankludkiewicz@gmail.com>
2021-04-08 22:42:00 +0200

    Update locallib.php

--- locallib.php
+++ locallib.php
@@ -145,7 +145,7 @@ function get_all_questions($categoryids) {
 function get_flagged_questions($categoryids) {
 	global $DB, $USER;
 	$DB->set_debug(true);
-	$results = $DB->get_records_sql("SELECT DISTINCT question.id
+	$results = $DB->get_fieldset_sql("SELECT DISTINCT question.id
 										FROM {question} AS question
 										JOIN {question_attempts} AS attempt ON attempt.questionid = question.id
 										JOIN {qbpractice_session} AS session ON session.questionusageid = attempt.questionusageid
